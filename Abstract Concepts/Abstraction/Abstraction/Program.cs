40eb3051106a76aaba1e0f2abaed9f51efc1621d
Akshatha <akshatha.patil.22@gmail.com>
2018-04-08 18:03:21 +0530

    Abstract samples

---
+++ Abstract Concepts/Abstraction/Abstraction/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test1 test1 = new Test1();
+            test1.Post();
+
+            //Base class type
+            Test1 test11 = new Test3();
+            test11.Post();
+
+            //Derived class1 type
+            Test2 test2 = new Test3();
+            test2.Post();
+
+            //Derived class 2 type
+            Test3 test3 = new Test3();
+            test3.Post();
+
+            //Base class value : Doesn't work. Cannot be cast down
+            //Test3 test = (Test3)new Test1();
+            //test.Post();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Abstract Concepts/Abstraction/Abstraction/Test1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction
+{
+    public class Test1
+    {
+        public virtual void Post()
+        {
+            Console.WriteLine("Calling from Test1");
+        }
+    }
+
+    public abstract class Test2 : Test1
+    {
+        public abstract override void Post();
+    }
+
+    public class Test3 : Test2
+    {
+        public override void Post()
+        {
+            Console.WriteLine("Calling from Test3");
+        }
+    }
+}
